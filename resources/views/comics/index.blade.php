0d8871afe947798b03f36918669b974f9a927abc
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-05-09 15:30:32 +0200

    creato il file show

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,7 +1,18 @@
 @extends('..base')
 @section('title', 'Laravel DC Comics')
 @section('content')
-    @foreach ($comics as $comic)
-        <h2>{{$comic['title']}}</h2>
-    @endforeach
+<div class="container">
+    <div class="row row-cols-5">
+        @foreach ($comics as $comic)
+        @dd($comics)
+        <div class="col">
+            <a href="{{route('comics.show', 'comics[id]')}}">
+                <div class="card">
+                    <h2>{{$comic['title']}}</h2>
+                </div>
+            </a>
+        </div>
+        @endforeach
+    </div>
+</div>
 @endsection
