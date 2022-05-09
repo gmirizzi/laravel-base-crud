b391233320b430081275312ca88515429281f109
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-05-09 14:36:21 +0200

    funzione index comics

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -14,7 +14,8 @@ class ComicController extends Controller
      */
     public function index()
     {
-        //
+        $comics=config('comics');
+        return view('comics.index', compact('comics'));
     }
 
     /**
