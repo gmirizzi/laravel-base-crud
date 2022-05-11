84a30be5e3cf9f329c830caf354f45ed975d7804
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-05-11 15:39:38 +0200

    modificate le routes

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-              <a class="navbar-brand" href="#"> 
+              <a class="navbar-brand" href="{{route('home')}}"> 
                   <img src="{{asset('images/dc-logo.png')}}" alt="Logo DC Comics" />
               </a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,10 +11,7 @@
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                   <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link active" href="#">Comics</a>
+                    <a class="nav-link active" href="{{route('comics.index')}}">Comics</a>
                   </li>
                 </ul>
                 <form class="d-flex">
