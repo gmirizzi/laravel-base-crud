c89cd498d4a21a2ed4ee78773f77196d77714a5c
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-05-11 14:42:51 +0200

    creato il form

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,5 +1,37 @@
 @extends('..base')
 @section('title', 'Add a new comic')
 @section('content')
-ciao
+<div class="container p-5">
+    <form>
+        <div class="mb-3">
+          <label for="title" class="form-label">Title</label>
+          <input type="text" class="form-control" id="title">
+        </div>
+        <div class="mb-3">
+            <label for="description" class="form-label">Description</label>
+            <textarea class="form-control" id="description"></textarea>
+        </div>
+        <div class="mb-3">
+            <label for="thumb" class="form-label">Image URL</label>
+            <input type="url" class="form-control" id="thumb">
+        </div>
+        <div class="mb-3">
+            <label for="price" class="form-label">Price</label>
+            <input type="number" class="form-control" id="price" placeholder="0.00£" step='0.01' min='0'>
+        </div>
+        <div class="mb-3">
+            <label for="series" class="form-label">Series</label>
+            <input type="text" class="form-control" id="series">
+        </div>
+        <div class="mb-3">
+            <label for="sale_date" class="form-label">Sale date</label>
+            <input type="date" class="form-control" id="sale_date">
+        </div>    
+        <div class="mb-3">
+            <label for="type" class="form-label">Type</label>
+            <input type="text" class="form-control" id="type">
+        </div>
+        <button type="submit" class="btn btn-primary">Add</button>
+      </form>
+</div>
 @endsection
