24f0175800935759dd1a3a5ac20b851ca22b061a
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-05-12 15:04:38 +0200

    aggiunto jumbotron, thumb e title allo show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,6 +1,14 @@
 @extends('..base')
 @section('title', 'Laravel DC Comics | '. $comic->title)
 @section('content')
+<div id="jumbotron"></div>
+<div id="topbar">
+    <div class="container position-relative h-100">
+        <div class="card">
+            <img src="{{$comic->thumb}}" alt="{{$comic->title}}" class="card-img-top">
+        </div>
+    </div>
+</div>
 <div class="container">
     <h2>{{$comic->title}}</h2>
 </div>
